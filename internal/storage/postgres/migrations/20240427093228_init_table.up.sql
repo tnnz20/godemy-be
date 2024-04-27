d3e0a3b7654e0eaa7787e8151ab9b3805fad4aa2
tnnz20 <kaitora200@gmail.com>
2024-04-27 23:40:19 +0800

    migrations: add schema for migrations

---
+++ internal/storage/postgres/migrations/20240427093228_init_table.up.sql
@@ -0,0 +1,64 @@
+CREATE TYPE user_role as enum('student', 'teacher');
+CREATE TYPE user_gender as enum('male', 'female');
+
+CREATE EXTENSION "uuid-ossp";
+
+CREATE TABLE users(
+    id UUID DEFAULT uuid_generate_v4(),
+    email VARCHAR(255) not null,
+    password VARCHAR(255) not null,
+    role user_role DEFAULT 'student',
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    deleted_at TIMESTAMP,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE profile(
+    id UUID DEFAULT uuid_generate_v4(),
+    name VARCHAR(255) not null,
+    date Date,
+    address VARCHAR(255),
+    gender user_gender,
+    profile_img VARCHAR(255),
+    users_id UUID not null,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (id),
+    FOREIGN KEY (users_id) REFERENCES users(id)
+);
+
+CREATE TABLE teacher(
+    id UUID DEFAULT uuid_generate_v4(),
+    users_id UUID not null,
+    PRIMARY KEY (id),
+    FOREIGN KEY (users_id) REFERENCES users(id)
+);
+
+CREATE TABLE class(
+    id UUID DEFAULT uuid_generate_v4(),
+    teacher_id UUID not null,
+    code_class VARCHAR(10) UNIQUE,
+    PRIMARY KEY (id),
+    FOREIGN KEY (teacher_id) REFERENCES teacher(id)
+);
+
+CREATE TABLE student(
+    id UUID DEFAULT uuid_generate_v4(),
+    users_id UUID not null,
+    class_id UUID,
+    threshold INT DEFAULT 0,
+    PRIMARY KEY (id),
+    FOREIGN KEY (users_id) REFERENCES users(id),
+    FOREIGN KEY (class_id) REFERENCES class(id)
+);
+
+CREATE TABLE assessment(
+    id UUID DEFAULT uuid_generate_v4(),
+    student_id UUID not null,
+    assessment_value int,
+    assessment_code varchar(5),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (student_id) REFERENCES student(id)
+);
