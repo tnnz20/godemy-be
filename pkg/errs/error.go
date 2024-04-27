2b485a79ba7be613630f0779f9fb09f83c659927
tnnz20 <kaitora200@gmail.com>
2024-04-28 02:07:58 +0800

    feat: add pkg error

---
+++ pkg/errs/error.go
@@ -0,0 +1,14 @@
+package errs
+
+import "errors"
+
+var (
+	ErrInvalidEmail  = errors.New("invalid email")
+	ErrEmailRequired = errors.New("email required")
+
+	ErrPasswordRequired      = errors.New("password required")
+	ErrInvalidLengthPassword = errors.New("password must be at least 8 characters")
+
+	ErrInvalidRole  = errors.New("invalid role")
+	ErrRoleRequired = errors.New("role required")
+)
