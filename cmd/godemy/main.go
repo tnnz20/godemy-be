92f452ec89ce77a8542d1ab907b11827b9643f39
tnnz20 <kaitora200@gmail.com>
2024-04-29 00:38:22 +0800

    feat:initialize Fiber router and handle authentication

--- cmd/godemy/main.go
+++ cmd/godemy/main.go
@@ -2,9 +2,41 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/tnnz20/godemy-be/config"
+	auth "github.com/tnnz20/godemy-be/internal/apps/auth/base"
+	"github.com/tnnz20/godemy-be/internal/storage/postgres"
 )
 
 func main() {
-	fmt.Println("Hello World Godemy-be v2")
+
+	filename := "config/config-local.yaml"
+	if err := config.Load(filename); err != nil {
+		panic(err)
+	}
+
+	cfg := config.Cfg
+
+	db, err := postgres.NewConnection(cfg.Database.Postgres)
+	if err != nil {
+		panic(err)
+	}
+
+	defer db.Close()
+
+	if err := db.Migrate(); err != nil {
+		panic(err)
+	}
+
+	router := fiber.New(fiber.Config{
+		AppName: cfg.App.Name,
+	})
+
+	// Init router
+	auth.Init(router, db.GetDB(), cfg.App.Encryption.JWTSecret)
+
+	port := fmt.Sprintf(":%s", cfg.App.Port)
+	router.Listen(port)
 
 }
