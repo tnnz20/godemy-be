c8c6dacc19f50572ff0c59307db15a92df294167
tnnz20 <kaitora200@gmail.com>
2024-04-29 00:30:10 +0800

    feat: add pkg response to handle response

---
+++ pkg/response/response.go
@@ -0,0 +1,72 @@
+package response
+
+import (
+	"net/http"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func SuccessCreated(c *fiber.Ctx, data interface{}) error {
+	return c.Status(http.StatusCreated).JSON(SuccessCreatedMessage{
+		Code:    http.StatusCreated,
+		Message: StatusSuccessCreatedName,
+	})
+}
+
+func SuccessOK(c *fiber.Ctx, data interface{}) error {
+	return c.Status(http.StatusOK).JSON(SuccessOKMessage{
+		Code:    http.StatusOK,
+		Message: StatusSuccessOK,
+		Data:    data,
+	})
+}
+
+func ErrorBadRequest(c *fiber.Ctx, err error) error {
+	return c.Status(http.StatusBadRequest).JSON(ErrorMessage{
+		Code: http.StatusBadRequest,
+		Error: &ErrorFormat{
+			ErrorName:        StatusBadRequestErrorName,
+			ErrorDescription: err.Error(),
+		},
+	})
+}
+
+func ErrorNotFound(c *fiber.Ctx, err error) error {
+	return c.Status(http.StatusNotFound).JSON(ErrorMessage{
+		Code: http.StatusNotFound,
+		Error: &ErrorFormat{
+			ErrorName:        StatusNotFound,
+			ErrorDescription: err.Error(),
+		},
+	})
+}
+
+func ErrorConflict(c *fiber.Ctx, err error) error {
+	return c.Status(http.StatusConflict).JSON(ErrorMessage{
+		Code: http.StatusConflict,
+		Error: &ErrorFormat{
+			ErrorName:        StatusConflict,
+			ErrorDescription: err.Error(),
+		},
+	})
+}
+
+func ErrorUnauthorized(c *fiber.Ctx) error {
+	return c.Status(http.StatusUnauthorized).JSON(ErrorMessage{
+		Code: http.StatusUnauthorized,
+		Error: &ErrorFormat{
+			ErrorName:        StatusUnauthorizedErrorName,
+			ErrorDescription: StatusUnauthorizedErrorDescription,
+		},
+	})
+}
+
+func InternalServerError(c *fiber.Ctx, err error) error {
+	return c.Status(http.StatusBadRequest).JSON(ErrorMessage{
+		Code: http.StatusInternalServerError,
+		Error: &ErrorFormat{
+			ErrorName:        StatusInternalServerName,
+			ErrorDescription: err.Error(),
+		},
+	})
+}
---
+++ pkg/response/response_constant.go
@@ -0,0 +1,32 @@
+package response
+
+const (
+	StatusInternalServerName  = "internal_server_error"
+	InternalServerDescription = "The server is unable to complete your request"
+)
+
+const (
+	StatusUnauthorizedErrorName        = "access_denied"
+	StatusUnauthorizedErrorDescription = "Authorization failed by filter."
+)
+
+const (
+	StatusNotFound            = "not_found"
+	StatusNotFoundDescription = "The requested resource is not found"
+)
+
+const (
+	StatusConflict            = "conflict"
+	StatusConflictDescription = "The request could not be completed due to a conflict with the current state of the target resource"
+)
+
+const (
+	StatusBadRequestErrorName        = "bad_request"
+	StatusBadRequestErrorDescription = "Your request resulted in error"
+)
+
+const (
+	StatusSuccessCreatedName = "success_created"
+	StatusSuccessOK          = "success_ok"
+	StatusSuccessLogin       = "success_login"
+)
