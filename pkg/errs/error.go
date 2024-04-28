d21f4d89d924add62dd009c1fd6650a4b4e2ab51
tnnz20 <kaitora200@gmail.com>
2024-04-28 12:01:17 +0800

    feat: add new errors

--- pkg/errs/error.go
+++ pkg/errs/error.go
@@ -6,6 +6,9 @@ var (
 	ErrInvalidEmail  = errors.New("invalid email")
 	ErrEmailRequired = errors.New("email required")
 
+	ErrInvalidLengthName = errors.New("name must be at least 3 characters")
+	ErrNameRequired      = errors.New("name required")
+
 	ErrPasswordRequired      = errors.New("password required")
 	ErrInvalidLengthPassword = errors.New("password must be at least 8 characters")
 
