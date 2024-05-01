6689bf6458388012a4b84bb1069f6715670cf698
tnnz20 <kaitora200@gmail.com>
2024-05-01 21:53:26 +0800

    migrations: update table class and change name into courses

--- internal/storage/postgres/migrations/20240427093228_init_table.up.sql
+++ internal/storage/postgres/migrations/20240427093228_init_table.up.sql
@@ -35,10 +35,13 @@ CREATE TABLE teacher(
     FOREIGN KEY (users_id) REFERENCES users(id)
 );
 
-CREATE TABLE class(
+CREATE TABLE courses(
     id UUID DEFAULT uuid_generate_v4(),
+    course_name VARCHAR(255),
+    course_code VARCHAR(10) UNIQUE,
     teacher_id UUID not null,
-    code_class VARCHAR(10) UNIQUE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (id),
     FOREIGN KEY (teacher_id) REFERENCES teacher(id)
 );
@@ -46,21 +49,21 @@ CREATE TABLE class(
 CREATE TABLE student(
     id UUID DEFAULT uuid_generate_v4(),
     users_id UUID not null,
-    class_id UUID,
+    course_id UUID,
     threshold INT DEFAULT 0,
     PRIMARY KEY (id),
     FOREIGN KEY (users_id) REFERENCES users(id),
-    FOREIGN KEY (class_id) REFERENCES class(id)
+    FOREIGN KEY (course_id) REFERENCES courses(id)
 );
 
 CREATE TABLE assessment(
     id UUID DEFAULT uuid_generate_v4(),
     student_id UUID not null,
-    class_id UUID not null,
+    course_id UUID not null,
     assessment_value int,
     assessment_code varchar(5),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (student_id) REFERENCES student(id),
-    FOREIGN KEY (class_id) REFERENCES class(id)
+    FOREIGN KEY (course_id) REFERENCES courses(id)
 );
