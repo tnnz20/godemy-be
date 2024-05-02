75af1bdc93483cb14ba0f7a81982049c70d14007
tnnz20 <kaitora200@gmail.com>
2024-05-02 11:16:02 +0800

    feat: add random seed

--- pkg/helpers/random_string.go
+++ pkg/helpers/random_string.go
@@ -1,13 +1,17 @@
 package helpers
 
-import "math/rand"
+import (
+	"hash/maphash"
+	"math/rand"
+)
 
 const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 
 func GenerateRandomString(n int) string {
+	r := rand.New(rand.NewSource(int64(new(maphash.Hash).Sum64())))
 	b := make([]byte, n)
 	for i := range b {
-		b[i] = letterBytes[rand.Intn(len(letterBytes))]
+		b[i] = letterBytes[r.Intn(len(letterBytes))]
 	}
 	return string(b)
 }
