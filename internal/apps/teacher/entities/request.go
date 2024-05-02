ba935582895659e99bffc4ded5e5f9949e2f6597
tnnz20 <kaitora200@gmail.com>
2024-05-02 15:58:40 +0800

    feat(Teacher): add struct literal

--- internal/apps/teacher/entities/request.go
+++ internal/apps/teacher/entities/request.go
@@ -3,13 +3,17 @@ package entities
 import "github.com/google/uuid"
 
 type GetTeacherIdByUserIdRequest struct {
-	UserId uuid.UUID
+	UserId uuid.UUID `json:"user_id"`
 }
 
-type GetCourseByTeacherId struct {
-	UserId uuid.UUID
+type GetCourseByTeacherIdRequest struct {
+	UserId uuid.UUID `json:"user_id"`
+}
+
+type GetCourseByCourseCodeRequest struct {
+	CourseCode string `json:"course_code"`
 }
 
 type CreateCourseRequest struct {
-	UserId uuid.UUID
+	UserId uuid.UUID `json:"user_id"`
 }
--- internal/apps/teacher/entities/response.go
+++ internal/apps/teacher/entities/response.go
@@ -7,14 +7,24 @@ import (
 )
 
 type GetTeacherIdByUserIdResponse struct {
-	Id     uuid.UUID
-	UserId uuid.UUID
+	ID     uuid.UUID `json:"id"`
+	UserId uuid.UUID `json:"user_id"`
 }
 
 type GetCourseByTeacherIdResponse struct {
-	Id         uuid.UUID
-	TeacherId  uuid.UUID
-	CodeCourse string
-	CreatedAt  time.Time
-	UpdatedAt  time.Time
+	ID         uuid.UUID `json:"id"`
+	TeacherId  uuid.UUID `json:"teacher_id"`
+	CourseName string    `json:"course_name"`
+	CourseCode string    `json:"course_code"`
+	CreatedAt  time.Time `json:"created_at"`
+	UpdatedAt  time.Time `json:"updated_at"`
+}
+
+type GetCourseByCourseCodeResponse struct {
+	ID         uuid.UUID `json:"id"`
+	TeacherId  uuid.UUID `json:"teacher_id"`
+	CourseName string    `json:"course_name"`
+	CourseCode string    `json:"course_code"`
+	CreatedAt  time.Time `json:"created_at"`
+	UpdatedAt  time.Time `json:"updated_at"`
 }
