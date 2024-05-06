b13ce3f04c28ebbc0bfb420deaed7276078759a3
tnnz20 <kaitora200@gmail.com>
2024-05-06 17:46:45 +0800

    migrations: refactor table

--- internal/storage/postgres/migrations/20240427093228_init_table.up.sql
+++ internal/storage/postgres/migrations/20240427093228_init_table.up.sql
@@ -1,56 +1,44 @@
-CREATE TYPE user_role as enum('student', 'teacher');
-CREATE TYPE user_gender as enum('male', 'female');
-
 CREATE EXTENSION "uuid-ossp";
 
 CREATE TABLE users(
     id UUID DEFAULT uuid_generate_v4(),
     email VARCHAR(255) not null,
     password VARCHAR(255) not null,
-    role user_role DEFAULT 'student',
+    name VARCHAR(255) not null,
+    date Date,
+    address VARCHAR(255),
+    gender VARCHAR(255),
+    profile_img VARCHAR(255),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     deleted_at TIMESTAMP,
     PRIMARY KEY (id)
 );
 
-CREATE TABLE profile(
-    id UUID DEFAULT uuid_generate_v4(),
-    name VARCHAR(255) not null,
-    date Date,
-    address VARCHAR(255),
-    gender user_gender,
-    profile_img VARCHAR(255),
+CREATE TABLE roles(
     users_id UUID not null,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY (id),
-    FOREIGN KEY (users_id) REFERENCES users(id)
-);
-
-CREATE TABLE teacher(
-    id UUID DEFAULT uuid_generate_v4(),
-    users_id UUID not null,
-    PRIMARY KEY (id),
-    FOREIGN KEY (users_id) REFERENCES users(id)
+    role VARCHAR(255) not null,
+    PRIMARY KEY (users_id)
 );
 
 CREATE TABLE courses(
     id UUID DEFAULT uuid_generate_v4(),
+    users_id UUID not null,
     course_name VARCHAR(255),
     course_code VARCHAR(10) UNIQUE,
-    teacher_id UUID not null,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (id),
-    FOREIGN KEY (teacher_id) REFERENCES teacher(id)
+    FOREIGN KEY (users_id) REFERENCES users(id)
 );
 
-CREATE TABLE student(
+CREATE TABLE course_enrollment(
     id UUID DEFAULT uuid_generate_v4(),
     users_id UUID not null,
     course_id UUID,
-    threshold INT DEFAULT 0,
+    progress INT DEFAULT 0,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (id),
     FOREIGN KEY (users_id) REFERENCES users(id),
     FOREIGN KEY (course_id) REFERENCES courses(id)
@@ -58,12 +46,12 @@ CREATE TABLE student(
 
 CREATE TABLE assessment(
     id UUID DEFAULT uuid_generate_v4(),
-    student_id UUID not null,
+    users_id UUID not null,
     course_id UUID not null,
     assessment_value int,
-    assessment_code varchar(5),
+    assessment_code varchar(10),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (student_id) REFERENCES student(id),
+    FOREIGN KEY (users_id) REFERENCES users(id),
     FOREIGN KEY (course_id) REFERENCES courses(id)
 );
