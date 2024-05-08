a22caa8db2f2ba500b57b5feae037b285aff6bfa
tnnz20 <kaitora200@gmail.com>
2024-05-08 11:43:27 +0800

    feat(Users): add new request and response

--- internal/apps/users/entities/request.go
+++ internal/apps/users/entities/request.go
@@ -1,5 +1,7 @@
 package entities
 
+import "github.com/google/uuid"
+
 type RegisterPayload struct {
 	Name     string `json:"name"`
 	Email    string `json:"email"`
@@ -10,4 +12,12 @@ type RegisterPayload struct {
 type LoginPayload struct {
 	Email    string `json:"email"`
 	Password string `json:"password"`
+}
+
+type GetUserPayload struct {
+	ID uuid.UUID `json:"id"`
+}
+
+type UpdateUserPayload struct {
+	ID uuid.UUID `json:"id"`
 }
--- internal/apps/users/entities/response.go
+++ internal/apps/users/entities/response.go
@@ -1,5 +1,23 @@
 package entities
 
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
 type LoginResponse struct {
 	Token string `json:"token"`
+}
+
+type UserResponse struct {
+	ID         uuid.UUID `json:"id"`
+	Name       string    `json:"name"`
+	Email      string    `json:"email"`
+	Date       time.Time `json:"date"`
+	Address    string    `json:"address"`
+	Gender     string    `json:"gender"`
+	ProfileImg string    `json:"profile_img"`
+	CreatedAt  time.Time `json:"created_at"`
+	UpdatedAt  time.Time `json:"updated_at"`
 }
