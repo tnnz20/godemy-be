8bbfe8b857ad78462358f680cc4a4a04b7e39bd1
tnnz20 <kaitora200@gmail.com>
2024-05-08 11:25:34 +0800

    feat(Course): feat add insert course enrollment and find enrolment interface

--- internal/apps/courses/repository.go
+++ internal/apps/courses/repository.go
@@ -7,11 +7,12 @@ import (
 	"github.com/tnnz20/godemy-be/internal/apps/courses/entities"
 )
 
-// TODO: add insert course enrollment
+// TODO: update progress enrollment
 // Repository is the interface that provides the repository methods for courses.
 type Repository interface {
 	CreateCourse(ctx context.Context, course entities.Courses) (err error)
 	FindCourseByCourseCode(ctx context.Context, courseCode string) (course entities.Courses, err error)
 	FindCoursesByUsersIdWithPagination(ctx context.Context, usersId uuid.UUID, model entities.CoursesPagination) (courses []entities.Courses, err error)
-	// InsertCourseEnrollment(ctx context.Context, enrollment entities.Enrollment) (err error)
+	InsertCourseEnrollment(ctx context.Context, enrollment entities.Enrollment) (err error)
+	FindCourseEnrollmentByUsersId(ctx context.Context, usersId uuid.UUID) (enrollments entities.Enrollment, err error)
 }
--- internal/apps/courses/service.go
+++ internal/apps/courses/service.go
@@ -6,11 +6,13 @@ import (
 	"github.com/tnnz20/godemy-be/internal/apps/courses/entities"
 )
 
-// TODO: add insert course enrollment service
+// TODO: update progress enrollment
 
 // Service is the interface that provides the service methods for courses.
 type Service interface {
 	CreateCourse(ctx context.Context, req entities.CreateCoursePayload) (err error)
 	GetCourseByCourseCode(ctx context.Context, req entities.GetCourseByCourseCodePayload) (res entities.CourseResponse, err error)
 	GetCoursesByUsersIdWithPagination(ctx context.Context, req entities.GetCoursesByUsersIdWithPaginationPayload) (res []entities.CourseResponse, err error)
+	EnrollCourse(ctx context.Context, req entities.EnrollCoursePayload) (err error)
+	GetCourseEnrollmentByUsersId(ctx context.Context, req entities.GetCourseEnrollmentByUsersIdPayload) (res entities.CourseEnrollmentResponse, err error)
 }
