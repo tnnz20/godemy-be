0e467333408ac724c0184a4c3c1ecba1e98ee8b5
tnnz20 <kaitora200@gmail.com>
2024-05-12 15:48:11 +0800

    feat(Assessment): add testing

---
+++ internal/apps/assessment/entities/assessment_test.go
@@ -0,0 +1,42 @@
+package entities
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"github.com/tnnz20/godemy-be/pkg/errs"
+)
+
+func TestAssessmentEntities(t *testing.T) {
+	t.Run("Success validate assessment", func(t *testing.T) {
+		assessment := Assessment{
+			AssessmentValue: 90,
+			AssessmentCode:  "chap-4",
+		}
+
+		err := assessment.Validate()
+		require.Nil(t, err)
+	})
+
+	t.Run("Failed validate assessment, invalid assessment value", func(t *testing.T) {
+		assessment := Assessment{
+			AssessmentValue: 101,
+			AssessmentCode:  "chap-4",
+		}
+
+		err := assessment.Validate()
+		require.NotNil(t, err)
+		require.Equal(t, errs.ErrInvalidAssessmentValue, err)
+	})
+
+	t.Run("Failed validate assessment, assessment code must required", func(t *testing.T) {
+		assessment := Assessment{
+			AssessmentValue: 90,
+			AssessmentCode:  "",
+		}
+
+		err := assessment.Validate()
+		require.NotNil(t, err)
+		require.Equal(t, errs.ErrAssessmentCodeRequired, err)
+	})
+}
---
+++ internal/apps/assessment/service/assessment_service_test.go
@@ -0,0 +1,174 @@
+package service
+
+import (
+	"context"
+	"log"
+	"testing"
+
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/require"
+	"github.com/tnnz20/godemy-be/config"
+	"github.com/tnnz20/godemy-be/internal/apps/assessment"
+	"github.com/tnnz20/godemy-be/internal/apps/assessment/entities"
+	"github.com/tnnz20/godemy-be/internal/apps/assessment/repository"
+	"github.com/tnnz20/godemy-be/internal/storage/postgres"
+	"github.com/tnnz20/godemy-be/pkg/errs"
+)
+
+var svc assessment.Service
+
+func init() {
+	err := config.Load("../../../../config/config-local.yaml")
+	if err != nil {
+		panic(err)
+	}
+
+	db, err := postgres.NewConnection(config.Cfg.Database.Postgres)
+	if err != nil {
+		panic(err)
+	}
+
+	repo := repository.NewRepository(db.GetDB())
+	svc = NewService(repo)
+}
+
+const (
+	validUserId                string = "6286637a-3d6c-460a-b68a-956fd9553059"
+	validAssessmentChapterCode string = "chap-3"
+)
+
+func TestCreateAssessment(t *testing.T) {
+	t.Run("Success create assessment", func(t *testing.T) {
+		userId, err := uuid.Parse(validUserId)
+		if err != nil {
+			t.Error(err)
+		}
+
+		req := entities.CreateAssessmentRequest{
+			UserId:          userId,
+			AssessmentValue: 7,
+			AssessmentCode:  validAssessmentChapterCode,
+		}
+
+		err = svc.CreateAssessment(context.Background(), req)
+		require.Nil(t, err)
+	})
+
+	t.Run("Failed create assessment, course enrollment not found", func(t *testing.T) {
+		userId, err := uuid.Parse("f8739934-a08a-494c-a0da-3f66553819f2")
+		if err != nil {
+			t.Error(err)
+		}
+
+		req := entities.CreateAssessmentRequest{
+			UserId:          userId,
+			AssessmentValue: 80,
+			AssessmentCode:  validAssessmentChapterCode,
+		}
+
+		err = svc.CreateAssessment(context.Background(), req)
+		require.NotNil(t, err)
+		require.Equal(t, errs.ErrCourseEnrollmentNotFound, err)
+		log.Print(err)
+	})
+}
+
+func TestGetAssessment(t *testing.T) {
+	t.Run("Success get assessment", func(t *testing.T) {
+		userId, err := uuid.Parse(validUserId)
+		if err != nil {
+			t.Error(err)
+		}
+
+		req := entities.GetAssessmentRequest{
+			UserId: userId,
+		}
+
+		assessment, err := svc.GetAssessment(context.Background(), req)
+		require.Nil(t, err)
+		require.NotEmpty(t, assessment)
+		log.Print(assessment)
+	})
+
+	t.Run("Failed get assessment, assessment not found", func(t *testing.T) {
+		userId, err := uuid.Parse("6286637a-3d6c-460a-b68a-956fd9553058")
+		if err != nil {
+			t.Error(err)
+		}
+
+		req := entities.GetAssessmentRequest{
+			UserId: userId,
+		}
+
+		_, err = svc.GetAssessment(context.Background(), req)
+		require.NotNil(t, err)
+		require.Equal(t, errs.ErrAssessmentNotFound, err)
+		log.Print(err)
+	})
+
+	t.Run("Success get assessments", func(t *testing.T) {
+		userId, err := uuid.Parse(validUserId)
+		if err != nil {
+			t.Error(err)
+		}
+
+		req := entities.GetAssessmentRequest{
+			UserId: userId,
+		}
+
+		assessments, err := svc.GetAssessments(context.Background(), req)
+		require.Nil(t, err)
+		require.NotEmpty(t, assessments)
+		log.Print(assessments)
+	})
+
+	t.Run("Failed get assessments, assessment not found", func(t *testing.T) {
+		userId, err := uuid.Parse("6286637a-3d6c-460a-b68a-956fd9553058")
+		if err != nil {
+			t.Error(err)
+		}
+
+		req := entities.GetAssessmentRequest{
+			UserId: userId,
+		}
+
+		_, err = svc.GetAssessments(context.Background(), req)
+		require.NotNil(t, err)
+		require.Equal(t, errs.ErrAssessmentNotFound, err)
+		log.Print(err)
+	})
+
+	t.Run("Success get assessment by assessment code", func(t *testing.T) {
+		userId, err := uuid.Parse(validUserId)
+		if err != nil {
+			t.Error(err)
+		}
+
+		req := entities.GetAssessmentByAssessmentCodeRequest{
+			UserId:         userId,
+			AssessmentCode: validAssessmentChapterCode,
+		}
+
+		assessment, err := svc.GetAssessmentByAssessmentCode(context.Background(), req)
+		require.Nil(t, err)
+		require.NotEmpty(t, assessment)
+		log.Print(assessment)
+	})
+
+	t.Run("Failed get assessment by assessment code, assessment not found", func(t *testing.T) {
+		userId, err := uuid.Parse(validUserId)
+		if err != nil {
+			t.Error(err)
+		}
+
+		req := entities.GetAssessmentByAssessmentCodeRequest{
+			UserId:         userId,
+			AssessmentCode: "chap-10",
+		}
+
+		_, err = svc.GetAssessmentByAssessmentCode(context.Background(), req)
+		require.NotNil(t, err)
+		require.Equal(t, errs.ErrAssessmentNotFound, err)
+		log.Print(err)
+	})
+}
