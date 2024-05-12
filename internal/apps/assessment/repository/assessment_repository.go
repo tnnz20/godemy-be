2432f5087fa41fae59599352540bd7741147282a
tnnz20 <kaitora200@gmail.com>
2024-05-12 15:48:01 +0800

    feat(Assessment): implement interface

---
+++ internal/apps/assessment/repository/assessment_repository.go
@@ -0,0 +1,194 @@
+package repository
+
+import (
+	"context"
+	"database/sql"
+
+	"github.com/google/uuid"
+	"github.com/tnnz20/godemy-be/internal/apps/assessment"
+	"github.com/tnnz20/godemy-be/internal/apps/assessment/entities"
+)
+
+type repository struct {
+	db *sql.DB
+}
+
+func NewRepository(db *sql.DB) assessment.Repository {
+	return &repository{
+		db: db,
+	}
+}
+
+// CreateAssessment is a function to create a new assessment
+func (r *repository) CreateAssessment(ctx context.Context, assessment entities.Assessment) (err error) {
+	query := `
+	INSERT INTO assessment (
+		id,
+		users_id,
+		courses_id,
+		assessment_value,
+		assessment_code,
+		created_at,
+		updated_at
+	)
+	VALUES ($1, $2, $3, $4, $5, $6, $7)
+	`
+
+	stmt, err := r.db.PrepareContext(ctx, query)
+	if err != nil {
+		return err
+	}
+
+	defer stmt.Close()
+
+	_, err = stmt.ExecContext(ctx,
+		assessment.ID,
+		assessment.UsersId,
+		assessment.CoursesId,
+		assessment.AssessmentValue,
+		assessment.AssessmentCode,
+		assessment.CreatedAt,
+		assessment.UpdatedAt,
+	)
+
+	if err != nil {
+		return err
+	}
+	return
+}
+
+// FindAssessment is a function to get assessment by user id
+func (r *repository) FindAssessment(ctx context.Context, userId uuid.UUID) (assessment entities.Assessment, err error) {
+	query := `
+	SELECT 
+		id, 
+		users_id, 
+		courses_id, 
+		assessment_value, 
+		assessment_code, 
+		created_at, 
+		updated_at
+	FROM assessment
+	WHERE users_id = $1
+	`
+
+	err = r.db.QueryRowContext(ctx, query, userId).Scan(
+		&assessment.ID,
+		&assessment.UsersId,
+		&assessment.CoursesId,
+		&assessment.AssessmentValue,
+		&assessment.AssessmentCode,
+		&assessment.CreatedAt,
+		&assessment.UpdatedAt,
+	)
+
+	if err != nil {
+		return
+	}
+
+	return
+}
+
+// FindAssessments is a function to get all assessments by user id
+func (r *repository) FindAssessments(ctx context.Context, usersId uuid.UUID) (assessments []entities.Assessment, err error) {
+	query := `
+	SELECT 
+		id, 
+		users_id, 
+		courses_id, 
+		assessment_value, 
+		assessment_code, 
+		created_at, 
+		updated_at
+	FROM assessment
+	WHERE users_id = $1
+	`
+
+	rows, err := r.db.QueryContext(ctx, query, usersId)
+	if err != nil {
+		return
+	}
+
+	defer rows.Close()
+
+	for rows.Next() {
+		var assessment entities.Assessment
+		err = rows.Scan(
+			&assessment.ID,
+			&assessment.UsersId,
+			&assessment.CoursesId,
+			&assessment.AssessmentValue,
+			&assessment.AssessmentCode,
+			&assessment.CreatedAt,
+			&assessment.UpdatedAt,
+		)
+
+		if err != nil {
+			return
+		}
+
+		assessments = append(assessments, assessment)
+	}
+
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return
+}
+
+// FindAssessmentByAssessmentCode is a function to get assessment by user id and assessment code
+func (r *repository) FindAssessmentByAssessmentCode(ctx context.Context, usersId uuid.UUID, assessmentCode string) (assessment entities.Assessment, err error) {
+	query := `
+	SELECT 
+		id, 
+		users_id, 
+		courses_id, 
+		assessment_value, 
+		assessment_code, 
+		created_at, 
+		updated_at
+	FROM assessment
+	WHERE users_id = $1 AND assessment_code = $2
+	`
+
+	err = r.db.QueryRowContext(ctx, query, usersId, assessmentCode).Scan(
+		&assessment.ID,
+		&assessment.UsersId,
+		&assessment.CoursesId,
+		&assessment.AssessmentValue,
+		&assessment.AssessmentCode,
+		&assessment.CreatedAt,
+		&assessment.UpdatedAt,
+	)
+
+	if err != nil {
+		return
+	}
+
+	return
+}
+
+// FindCoursesEnrollment is a function to get course by user id
+func (r *repository) FindCoursesEnrollment(ctx context.Context, usersId uuid.UUID) (enrollment entities.Enrollment, err error) {
+	query := `
+	SELECT id, users_id, courses_id, progress, created_at, updated_at
+	FROM course_enrollment
+	WHERE users_id = $1
+	`
+
+	err = r.db.QueryRowContext(ctx, query, usersId).Scan(
+		&enrollment.ID,
+		&enrollment.UsersId,
+		&enrollment.CoursesId,
+		&enrollment.Progress,
+		&enrollment.CreatedAt,
+		&enrollment.UpdatedAt,
+	)
+
+	if err != nil {
+		return
+	}
+
+	return
+}
---
+++ internal/apps/assessment/service/assessment_service.go
@@ -0,0 +1,95 @@
+package service
+
+import (
+	"context"
+	"database/sql"
+	"errors"
+
+	"github.com/tnnz20/godemy-be/internal/apps/assessment"
+	"github.com/tnnz20/godemy-be/internal/apps/assessment/entities"
+	"github.com/tnnz20/godemy-be/pkg/errs"
+)
+
+type service struct {
+	assessment.Repository
+}
+
+func NewService(assessmentRepo assessment.Repository) assessment.Service {
+	return &service{
+		Repository: assessmentRepo,
+	}
+}
+
+// CreateAssessment is a function to create a new assessment
+func (s *service) CreateAssessment(ctx context.Context, req entities.CreateAssessmentRequest) (err error) {
+	courseEnrollment, err := s.Repository.FindCoursesEnrollment(ctx, req.UserId)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			err = errs.ErrCourseEnrollmentNotFound
+			return
+		}
+		return
+	}
+
+	NewAssessment := entities.NewAssessment(req.UserId, courseEnrollment.CoursesId, req.AssessmentValue, req.AssessmentCode)
+
+	if err = NewAssessment.Validate(); err != nil {
+		return
+	}
+	err = s.Repository.CreateAssessment(ctx, NewAssessment)
+	if err != nil {
+		return err
+	}
+	return
+}
+
+// GetAssessment is a function to get assessment by user id
+func (s *service) GetAssessment(ctx context.Context, req entities.GetAssessmentRequest) (res entities.AssessmentResponse, err error) {
+	assessment, err := s.Repository.FindAssessment(ctx, req.UserId)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			err = errs.ErrAssessmentNotFound
+			return
+		}
+		return res, err
+	}
+
+	res = entities.AssessmentResponse(assessment)
+
+	return
+}
+
+// GetAssessments is a function to get all assessments by user id
+func (s *service) GetAssessments(ctx context.Context, req entities.GetAssessmentRequest) (res []entities.AssessmentResponse, err error) {
+	assessments, err := s.Repository.FindAssessments(ctx, req.UserId)
+	if err != nil {
+		return []entities.AssessmentResponse{}, err
+	}
+
+	if len(assessments) == 0 {
+		err = errs.ErrAssessmentNotFound
+		return
+	}
+
+	for _, assessment := range assessments {
+		res = append(res, entities.AssessmentResponse(assessment))
+	}
+
+	return
+}
+
+// GetAssessmentByAssessmentCode is a function to get assessment by assessment code
+func (s *service) GetAssessmentByAssessmentCode(ctx context.Context, req entities.GetAssessmentByAssessmentCodeRequest) (res entities.AssessmentResponse, err error) {
+	assessment, err := s.Repository.FindAssessmentByAssessmentCode(ctx, req.UserId, req.AssessmentCode)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			err = errs.ErrAssessmentNotFound
+			return
+		}
+		return res, err
+	}
+
+	res = entities.AssessmentResponse(assessment)
+
+	return
+}
