c16f8df1502c1cf70f4cc213522a2c81084e08da
tnnz20 <kaitora200@gmail.com>
2024-05-21 18:22:45 +0800

    refactor: update query parameter name in GetAssessmentByAssessmentCodeRequest struct

--- internal/apps/assessment/entities/request.go
+++ internal/apps/assessment/entities/request.go
@@ -14,7 +14,7 @@ type GetAssessmentRequest struct {
 
 type GetAssessmentByAssessmentCodeRequest struct {
 	UsersId        uuid.UUID `json:"users_id"`
-	AssessmentCode string    `params:"assessment_code"`
+	AssessmentCode string    `query:"assessment_code"`
 }
 
 type CreateUsersAssessmentRequest struct {
