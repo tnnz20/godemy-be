fc514855f37921d11945200d7c0e0afdd5eb397b
tnnz20 <kaitora200@gmail.com>
2024-06-12 13:30:10 +0800

    refactor(Users): update struct field

--- internal/apps/users/entities/request.go
+++ internal/apps/users/entities/request.go
@@ -1,6 +1,8 @@
 package entities
 
-import "github.com/google/uuid"
+import (
+	"github.com/google/uuid"
+)
 
 type RegisterPayload struct {
 	Name     string `json:"name"`
@@ -15,9 +17,14 @@ type LoginPayload struct {
 }
 
 type GetUserPayload struct {
-	ID uuid.UUID `json:"id"`
+	ID uuid.UUID `json:"id" query:"id"`
 }
 
 type UpdateUserPayload struct {
-	ID uuid.UUID `json:"id"`
+	ID         uuid.UUID `json:"id"`
+	Name       string    `json:"name"`
+	Date       string    `json:"date"`
+	Address    string    `json:"address"`
+	Gender     string    `json:"gender"`
+	ProfileImg string    `json:"profile_img"`
 }
